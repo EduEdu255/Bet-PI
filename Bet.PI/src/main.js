bb520001b093371ea04ca68d3d533c637e00ec99
Hans Christian Alencar <christianhcoa@gmail.com>
2024-11-18 21:31:01 -0300

    Instalação da biblioteca de toasts

--- Bet.PI/src/main.js
+++ Bet.PI/src/main.js
@@ -3,5 +3,22 @@ import './assets/estilos/estilos_basicos.css'
 import router from './router.ts'
 import { createApp } from 'vue'
 import App from './App.vue'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+const options = {
+  position: 'top-right',
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: false,
+  closeButton: 'button',
+  icon: true,
+  rtl: false
+}
+
+createApp(App).use(router).use(Toast, options).mount('#app')
